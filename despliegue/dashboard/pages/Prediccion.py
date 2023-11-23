f3e6caf0c3688fc7e055e9da6e2c5d48abd4a5ca
SergioRojas86 <rojas.sergio86@hotmail.com>
2023-11-23 18:58:06 -0500

    Archivos para predicción

--- despliegue/dashboard/pages/Prediccion.py
+++ despliegue/dashboard/pages/Prediccion.py
@@ -409,12 +409,12 @@ def update_output(n_clicks, selected_date, selected_time, id_municipio, id_estad
     df_respuestas = pd.DataFrame(np.column_stack(list_respuestas),columns=Lista_col) 
 
     clean_data = limpiar_datos(df_respuestas)
-    clean_data = clean_data.reshape(1, -1)
+    #clean_data = clean_data.reshape(1, -1)
 
     with open('pages/GBC.pkl', 'rb') as file:
         modelo = pickle.load(file)
 
-    resultado_predict = modelo.predict_proba(clean_data)[:, 1] * 100 
+    #resultado_predict = modelo.predict_proba(clean_data)[:, 1] * 100 
 
     print("resultado")   
     
@@ -423,7 +423,7 @@ def update_output(n_clicks, selected_date, selected_time, id_municipio, id_estad
     
     # Crear un DataFrame de ejemplo con la palabra "prueba"
 
-    df_download = pd.DataFrame(df_respuestas)
+    df_download = pd.DataFrame(clean_data)
 
     # Guardar el DataFrame en un archivo CSV en memoria
     csv_data = df_download.to_csv(index=False, encoding='utf-8-sig')
@@ -437,8 +437,8 @@ def update_output(n_clicks, selected_date, selected_time, id_municipio, id_estad
     
     # Lógica de predicción (puedes cambiar esto según tu implementación real)
     
-    #resultado_prediccion = 27.75 +10 # Esto debe ser tu resultado de la predicción
-    resultado_prediccion = resultado_predict
+    resultado_prediccion = 27.75 +10 # Esto debe ser tu resultado de la predicción
+    #resultado_prediccion = resultado_predict
     
     return f"Resultado de la predicción: {resultado_prediccion:.2f}%" , download_link
 
